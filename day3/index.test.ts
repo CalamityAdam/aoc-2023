f994a3aa5858bc68106a9d67e690df16042e5667
Adam Sisk <adamsisk91@gmail.com>
2023-12-06 11:15:24 -0600

    solves day3 part2

--- day3/index.test.ts
+++ day3/index.test.ts
@@ -13,12 +13,13 @@ const sampleInput = [
   '.664.598..',
 ];
 // part numbers = 467, 35, 633, 617, 592, 755, 664, 598
+// const smallSampleInput = ['467..114..', '...*......', '..35..633.'];
 
-const sampleOutput = 4361;
+const sampleOutput = 467835;
 
 describe.only('day3', () => {
   describe('gearRatios', () => {
-    it('correctly sums the value of all the part numbers in the engine schematic', () => {
+    it('correctly sums the value of all the gear ratios in the engine schematic', () => {
       expect(gearRatios(sampleInput)).toBe(sampleOutput);
     });
   });
--- day3/index.ts
+++ day3/index.ts
@@ -1,83 +1,4 @@
 import { readFileSync } from 'fs';
-/*
-any number adjacent to a symbol, even diagonally, is a "part number" and should
-be included in your sum. (Periods (.) do not count as a symbol.)
-
-sample input:
-467..114..
-...*......
-..35..633.
-......#...
-617*......
-.....+.58.
-..592.....
-......755.
-...$.*....
-.664.598..
-
-part numbers = 467, 35, 633, 617, 592, 755, 664, 598
-
-sample output: 4361
-*/
-
-/*
-
-given a multi-line string input
-create empty array of allPartNumbers
-iterate over each line.
-for each line, iterate over each character
-
-if period, skip & continue
-🚀 could build some sort of mapping system here for blocks?
-
-if a number: start building a PartNumber, check if is valid
-
-step to next character, if another number how do we continue building the
-previous PartNumber?
-keep a "previous" state in the outer loop. initial null
-at beginning of inner loop, const partNumber: PartNumber = previous ? previous : new PartNumber
-
-if number: grab previous or new, concat id, check if isValid and set true if so otherwise skip, set previous = new
-
-❓ is it valid part number if it's adjacent another number but not adjacent a _symbol_ per se
-a literalist would say the instructions say any number adjacent a symbol, not "adjacent a number"
-
-
-at end of inner loop, push PartNumber
-outer loop just sets previous as null
-
-isValid function should take the original input an X and Y index (X being inner loop and Y being outer loop)
-set isValid = false;
-need to check 8 positions: x-1 y-1, x y-1, x+1 y-1, x-1 y, x+1 y, x-1 y+1, x y+1, x+1 y+1
-if any are symbol (regex check) then set isValid = true and return 
-or just return false at end of function and true if symbol is found
-
-
-after loops have completed, const validPartNumbers = allPartNumbers.filter(pn => pn.isValid);
-
-sum validPns
-
-another approach would be to iterate and search for symbols and then grab any numbers adjacent to the symbol
-
-🤔 potential clashing issue, storing duplicate partNumbers somehow. could hash an id based off indices to avoid duplication
-
-interface PartNumber {
-  id: string; // default: ''
-  isValid: boolean; // default: false
-}
-*/
-
-interface PartNumber {
-  id: string;
-  isValid: boolean;
-}
-
-function PartNumber(): PartNumber {
-  return {
-    id: '',
-    isValid: false,
-  };
-}
 
 /**
  *
@@ -85,63 +6,86 @@ function PartNumber(): PartNumber {
  * @returns
  */
 function gearRatios(input: string[]): number {
-  const allPartNumbers: PartNumber[] = [];
+  let gearRationSum = 0;
 
   for (let y = 0; y < input.length; ++y) {
     if (!input[y].length) {
       continue;
     }
-
-    // Y axis
-    let previous: PartNumber | null = null;
-
     for (let x = 0; x < input[y].length; ++x) {
       const char = input[y][x];
-      // X axis
-      // if is not a number continue
-      if (!/\d/.test(char)) {
-        continue;
-      }
+      if (char === '*') {
+        const [num1, num2] = findAdjacentPartNumbers(input, x, y);
 
-      const partNumber: PartNumber = previous ? previous : PartNumber();
-      partNumber.id += char;
-
-      // don't bother checking if we already know it's valid
-      if (!partNumber.isValid && isValid(input, x, y)) {
-        partNumber.isValid = true;
-      }
-
-      // if next char is not a number then push to allPns
-      if (!/\d/.test(input[y][x + 1])) {
-        // store this part number, clear out previous state
-        allPartNumbers.push(partNumber);
-        previous = null;
-      } else {
-        previous = partNumber;
+        if (num1 && num2) {
+          gearRationSum += num1 * num2;
+        }
       }
     }
   }
 
-  const validPartNumbers = allPartNumbers.filter((pn) => pn.isValid);
-  const sum = validPartNumbers.reduce((sum, pn) => sum + Number(pn.id), 0);
-  return sum;
+  return gearRationSum;
 }
 
-function isValid(arr: string[], x: number, y: number): boolean {
-  const checks = [
-    arr[y - 1]?.[x - 1],
-    arr[y - 1]?.[x],
-    arr[y - 1]?.[x + 1],
-    arr[y]?.[x - 1],
-    arr[y]?.[x + 1],
-    arr[y + 1]?.[x - 1],
-    arr[y + 1]?.[x],
-    arr[y + 1]?.[x + 1],
+function findAdjacentPartNumbers(
+  arr: string[],
+  x: number,
+  y: number
+): [number, number] | [null, null] {
+  const adjacentCoords = [
+    [y - 1, x - 1],
+    [y - 1, x],
+    [y - 1, x + 1],
+    [y, x - 1],
+    [y, x + 1],
+    [y + 1, x - 1],
+    [y + 1, x],
+    [y + 1, x + 1],
   ];
 
-  const regex = /[+\*=\-&#\/%$@]/;
-  const isValid = checks.some((char: string) => char && regex.test(char));
-  return isValid;
+  const partNumbers: number[] = [];
+
+  for (const [adjY, adjX] of adjacentCoords) {
+    const partNumber = extractPartNumber(arr, adjX, adjY);
+
+    if (partNumber !== null && !partNumbers.includes(partNumber)) {
+      partNumbers.push(partNumber);
+    }
+  }
+
+  console.log(partNumbers);
+  if (partNumbers.length === 2) {
+    return [partNumbers[0], partNumbers[1]];
+  }
+
+  return [null, null];
+}
+
+function extractPartNumber(arr: string[], x: number, y: number): number | null {
+  if (y < 0 || y >= arr.length || x < 0 || x > arr[y].length) {
+    return null;
+  }
+
+  const char = arr[y][x];
+
+  if (/\d/.test(char)) {
+    // extract the full part number
+    let fullNumber = char;
+
+    // check left
+    for (let i = x - 1; i >= 0 && /\d/.test(arr[y][i]); --i) {
+      fullNumber = arr[y][i] + fullNumber;
+    }
+
+    // check right
+    for (let i = x + 1; i < arr[y].length && /\d/.test(arr[y][i]); ++i) {
+      fullNumber += arr[y][i];
+    }
+
+    return parseInt(fullNumber);
+  }
+
+  return null;
 }
 
 export { gearRatios };
